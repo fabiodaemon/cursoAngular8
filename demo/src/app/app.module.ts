92ffcde32c915b3e6628a39979eaf3ce92825f36
fabiodaemon <44199988+fabiodaemon@users.noreply.github.com>
2019-11-10 21:39:38 -0300

    two-data binding

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FuncionarioListaComponent } from './funcionario-lista/funcionario-lista.component';
@@ -13,7 +14,8 @@ import { FuncionarioService } from './funcionario.service';
     FuncionarioDetalheComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [FuncionarioService],
   bootstrap: [AppComponent]
