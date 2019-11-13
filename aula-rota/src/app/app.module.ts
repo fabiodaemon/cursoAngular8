3225c7e582a09c6e790b69d39d774557ec4bf1df
fabiodaemon <44199988+fabiodaemon@users.noreply.github.com>
2019-11-12 21:54:00 -0300

    angular routes

---
+++ aula-rota/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FuncionarioComponent } from './funcionario/funcionario.component';
+import { DepartamentoComponent } from './departamento/departamento.component';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FuncionarioComponent,
+    DepartamentoComponent,
+    HomeComponent,
+    PagenotfoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
